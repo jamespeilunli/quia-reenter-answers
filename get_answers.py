316cfdde9c50746d6640ecc3e36675bd67d32838
James Li <jamespeilunli@gmail.com>
2021-10-31 13:45:17 -0700

    minor comments about html parsing

--- get_answers.py
+++ get_answers.py
@@ -7,8 +7,8 @@ def get_answers(answer_page_filename):
     
     answers = []
     question_number = 1
-    for question in soup.find_all("li"): 
-        if question.div.font["color"] == "green":
+    for question in soup.find_all("li"): # li elements contain answers
+        if question.div.font["color"] == "green": # <div><font color="green" ... means answer is correct
             answers.append(question.div.font.span.get_text())
         else: # color is red
             answers.append("")
